3ff8772961d2eca5af8bfb3e4f719deae3cfe189
BiancaTS <53481713+BiancaTS@users.noreply.github.com>
2023-11-15 10:03:43 -0300

    Estratégias
    Atividades. Exercício de faculdade de Ciência da Computação. Paradigmas de Linguagem de Programação em Python - Python Estruturado - Emprego e estruturas de decisão e repetição.

---
+++ Estratégia 01.py
@@ -0,0 +1,8 @@
+#Estratégia 01
+a = 10
+b = 20
+if(a>b):
+    maior = a
+else:
+    maior = b
+print('O maior número é:{}'.format(maior))
---
+++ Estratégia 02.py
@@ -0,0 +1,7 @@
+#Estratégia 02
+a = 10
+b = 20
+maior = a
+if(b>maior):
+    maior = b
+print('O maior número é:{}'.format(maior))
